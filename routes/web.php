636ae0df43c6d6fef665c5b0deb540ec618508db
yushigi.hong <yushigi.hong@rakuten.com>
2018-06-20 05:56:57 +0000

    user registration

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,7 @@ Route::get('/', function () {
     
 Route::get('/', 'WelcomeController@index');
 
+// User registration
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
 });
